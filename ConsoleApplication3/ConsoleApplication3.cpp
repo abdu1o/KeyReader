755feb7b8b908fa84f099a6c92aacd8eece0af6a
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-03-16 11:55:09 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <conio.h>
+
+int main() 
+{
+    char ch;
+    std::cout << "Press any key to display its ASCII code." << std::endl;
+    while (true) 
+    {
+        ch = _getch();
+        std::cout << "The ASCII code for " << ch << " is " << static_cast<int>(ch) << std::endl;
+    }
+    return 0;
+}
